3b64a51217e632362d915844b7009e0bd28c8de1
Enigma <626201515@qq.com>
2025-03-27 02:31:58 +0800

    Add files via upload

---
+++ image_reconstruction/prepare.sh
@@ -0,0 +1,26 @@
+mv ./AquaScan_data/raw_data ./raw_data
+mv ./AquaScan_data/raw_labels ./raw_labels
+mv ./AquaScan_data/scripts/0229_recover.sh ./0229_recover.sh
+mv ./AquaScan_data/scripts/0807_recover.sh ./0807_recover.sh
+mv ./AquaScan_data/scripts/0814_recover.sh ./0814_recover.sh
+mv ./AquaScan_data/scripts/0821_recover.sh ./0821_recover.sh
+
+mv ./AquaScan_data/scripts/label_0229.sh ../label_0229.sh
+mv ./AquaScan_data/scripts/label_0807.sh ../label_0807.sh
+mv ./AquaScan_data/scripts/label_0814.sh ../label_0814.sh
+mv ./AquaScan_data/scripts/label_0821.sh ../label_0821.sh
+
+mv ./AquaScan_data/scripts/infe_0229.sh ../infe_0229.sh
+mv ./AquaScan_data/scripts/infe_0807.sh ../infe_0807.sh
+mv ./AquaScan_data/scripts/infe_0814.sh ../infe_0814.sh
+mv ./AquaScan_data/scripts/infe_0821.sh ../infe_0821.sh
+
+mv ./AquaScan_data/scripts/remove_history_0229.sh ../remove_history_0229.sh
+mv ./AquaScan_data/scripts/remove_history_0807.sh ../remove_history_0807.sh
+mv ./AquaScan_data/scripts/remove_history_0814.sh ../remove_history_0814.sh
+mv ./AquaScan_data/scripts/remove_history_0821.sh ../remove_history_0821.sh
+
+mv ./AquaScan_data/scripts/remove_results.sh ../remove_results.sh
+mkdir ./recover
+mkdir ./recover/txt ./recover/img
+rm -r AquaScan_data
